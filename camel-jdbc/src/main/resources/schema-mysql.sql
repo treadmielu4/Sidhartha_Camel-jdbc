f85861f58bc23b563b65ee6c21dcd03f289bcde8
Treadmielu4 <treadmielu4@gmail.com>
2020-11-26 11:13:44 +0530

    Add files via upload

---
+++ camel-jdbc/src/main/resources/schema-mysql.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee (
+  empId VARCHAR(10) NOT NULL,
+  empName VARCHAR(100) NOT NULL
+);
+
+insert into employee values( emp001, "emp1");
+insert into employee values( emp002, "emp2");
+insert into employee values( emp003, "emp3");
+insert into employee values( emp004, "emp4");
+insert into employee values( emp005, "emp5");
